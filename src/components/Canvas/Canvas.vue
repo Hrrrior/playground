e5780ab95df9e2c7b75629e5df4b418ce8138ab5
Hrrrior <henri.helisma@outlook.com>
2021-09-08 09:54:09 +0300

    push

---
+++ src/components/Canvas/Canvas.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="c-canvas" ref="canvas">{{renderBox}}</div>
+</template>
+<script lang='js' src='./Canvas.js'/>
+<style lang='scss' src='./Canvas.scss'/>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Canvas/>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Canvas from '@/components/Canvas/Canvas.vue'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { Canvas }
 }
 </script>
