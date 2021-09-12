75dd480c7ab4fab1ce6699f0923544edb4641ea4
Hrrrior <henri.helisma@outlook.com>
2021-09-12 20:29:58 +0300

    push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="l-section l-section--sec1"><v-clock/></section>
-  <section class="l-section l-section--sec2">Test </section>
+  <section class="l-section l-section--sec2 grid"> <div class="text-container grid--sidebarRight"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nam debitis provident officiis, dolores cumque deserunt, possimus iure cum, architecto adipisci molestiae. Repudiandae soluta incidunt doloribus! Dolor quo at et. Obcaecati tempora aspernatur inventore ad esse accusamus nam iure, est quo. Necessitatibus in pariatur minima vel consequatur placeat fugiat voluptas vero veritatis et accusantium, animi, modi unde a distinctio harum recusandae! Sapiente doloribus culpa minus suscipit natus mollitia perferendis facilis amet earum placeat reiciendis ab veritatis enim magnam ullam eaque deleniti libero dolore, eum dolorum praesentium vitae necessitatibus dolores iste. Incidunt quidem obcaecati unde? Illo itaque repellat quidem blanditiis aliquam. </div> </section>
 </template>
 
 <script>
